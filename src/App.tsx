cb9f1c546f0b3ffa4a55ec0a667ab76bc61900f7
Gar Diu <gar@Gars-MacBook-Pro.local>
2018-04-11 23:15:42 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './containers/Home/Home';
+import Login from './containers/Login/Login';
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+       
+        <Router>
+            <div>
+               <h2>Welcome to React Router </h2>
+               <ul>
+                  <li><Link to={'/'}>Home</Link></li>
+                  <li><Link to={'/Login'}>Login</Link></li>
+               </ul>
+               <hr />
+               
+               <Switch>
+                  <Route exact={true} path="/" component={Home} />
+                  <Route exact={true} path="/Login" component={Login} />
+               </Switch>
+            </div>
+         </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Login/Login.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+      </div>
+    );
+  }
+}
+
+export default Login;
